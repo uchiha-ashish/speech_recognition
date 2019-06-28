0386be2d32772c1b7b2fd7538ffee7b952f1e21e
Ashish Sonavane <ashishs8652@gmail.com>
2019-06-28 16:45:23 +0530

    real_time_plotting

---
+++ real_time_plotting/liveanimate.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 14:05:08 2019
+
+@author: tatras
+"""
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+# %matplotlib qt
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    pullData = open("/home/tatras/Desktop/freq_time.txt","r").read()
+    dataArray = pullData.split('\n')
+    xar = []
+    yar = []
+    for eachLine in dataArray:
+        if len(eachLine)>1:
+            x,y = eachLine.split(',')
+            xar.append(int(x))
+            yar.append(int(y))
+    ax1.clear()
+    ax1.plot(yar,xar)
+ani = animation.FuncAnimation(fig, animate)
+plt.show()
